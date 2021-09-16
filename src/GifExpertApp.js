2d3189ce8f9cfbcdab0d3e643a50fb4fc446c0f4
Cristian Morales <cr15moresp@gmail.com>
2021-09-16 15:37:41 -0500

    Se creo una lista ordenada de categorias

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 
 const GifExpertApp = () => {
+
+    // const categories = ['Warframe', 'God of war', 'Dark souls'];
+
+    const [categories, setCategories] = useState(['Warframe', 'God of war', 'Dark souls']);
+
+    const newGame = 'Assassins creed';
+    
+    const handleAdd = (event) => {
+        
+        // setCategories( [...categories, newGame] );
+        setCategories(  cats => [...cats, newGame] );
+
+    };
 
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button id="btnAgregar" onClick={ handleAdd } >Agregar</button>
+
+            <ol>
+                {
+                    categories.map( categorie => {
+                        return (
+                            // El key siempre sera ell mismo elemento que se coloca
+                            <li key={categorie} >{categorie}</li>
+                        )
+                    })
+                }
+            </ol>
         </>
     );
 
