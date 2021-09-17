0a5129537b1d3ec13c77b3afc05377197966966f
Cristian Morales <cr15moresp@gmail.com>
2021-09-17 16:57:19 -0500

    Animaciones aplicadas en cada gif

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -5,7 +5,7 @@ export const GifGridItem = ({ id, title='No title', url }) => {
     if ( title.trim() === '' ) title = 'No title';
     
     return (
-        <div className="card">
+        <div className="card animate__animated animate__bounce">
             <img src={ url } alt={title} />
             <p>{ title }</p>
         </div>
